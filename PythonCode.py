206c1684364e25fce9dbb6947d4bc9c4449482e1
Tatia Tsiklauri <tatiatsiklauri@Tatias-MacBook-Air.local>
2024-11-27 13:37:46 +0400

    პირველი კომიტi

---
+++ PythonCode.py
@@ -0,0 +1,12 @@
+# Running Python code
+hello = "Hello!"
+print(hello)
+print("adsfsdafsda")
+text = input("Type some code: ")
+print(text)
+
+c = 5
+giorgi = "bandzi"
+bandzi = "giorgi"
+
+print(5*(giorgi + " " +  bandzi + ","))
